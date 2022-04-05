f44ef9bb7a2fe1508409f7ac0bb5f13859d2caf0
Konstantin Beloglazov <ajoq@ya.ru>
2022-04-05 17:04:55 +0300

    Задача 1, dev

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,35 @@
 import Cart from '../service/Cart';
+import Movie from '../domain/Movie';
 
 test('new card should be empty', () => {
   const cart = new Cart();
 
   expect(cart.items.length).toBe(0);
+});
+
+test('add movie in cart', () => {
+  const cart = new Cart();
+  cart.add(new Movie(100, 'Мстители', 120, 2012, 'США', 'Avengers assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137));
+
+    const expected = {
+        "_items": [
+            {
+                "id": 100,
+                "name": "Мстители",
+                "price": 120,
+                "year": 2012,
+                "country": "США",
+                "slogan": "Avengers assemble!",
+                "genre": [
+                    "фантастика",
+                    "боевик",
+                    "фэнтези",
+                    "приключения"
+                ],
+                "time": 137
+            }
+        ]
+    }
+    
+  expect(cart).toEqual(expected);
 });
